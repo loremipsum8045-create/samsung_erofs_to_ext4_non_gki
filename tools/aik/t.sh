e5693545d2df5f6521227b7b799b8685103072e8
loremipsum8045-create <loremipsum8045@gmail.com>
2026-07-23 12:26:50 +0500

    Revert "fixed file size issues"
    This reverts commit 5a24e4d000d6a2d7dcda09128d59163a7fea6b45.

--- tools/aik/t.sh
+++ tools/aik/t.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-aik="${BASH_SOURCE:-$0}"
-aik="$(dirname "$(readlink -f "$aik")")"
+input_files=(./aik/ramdisk/fstab.*)
 
-input_files=("$aik"/ramdisk/fstab.*)
-
-sudo sed -i 's/erofs/ext4/g' "${input_files[@]}"
+sudo sed -i 's/erofs/ext4/g' $input_files
